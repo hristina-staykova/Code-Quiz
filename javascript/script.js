8f97230be16c567bd03fe4daa3f565ff3ac4dab3
Hristina Staykova <hristina.staykova@gmail.com>
2019-10-10 13:23:53 +0200

    for loop going through & displaying all questions

--- javascript/script.js
+++ javascript/script.js
@@ -38,11 +38,14 @@ function startQuestions() {
   var answerEl2 = document.getElementById("answer2");
   var answerEl3 = document.getElementById("answer3");
   var answerEl4 = document.getElementById("answer4");
-  question.textContent = questions[0].title;
-  answerEl1.textContent = "1. " + questions[0].choices[1];
-  answerEl2.textContent = "2. " + questions[0].choices[2];
-  answerEl3.textContent = "3. " + questions[0].choices[3];
-  answerEl4.textContent = "4. " + questions[0].choices[4];
+
+  for (i = 0; i < 4; i++) {
+    question.textContent = questions[i].title;
+    answerEl1.textContent = "1. " + questions[i].choices[0];
+    answerEl2.textContent = "2. " + questions[i].choices[1];
+    answerEl3.textContent = "3. " + questions[i].choices[2];
+    answerEl4.textContent = "4. " + questions[i].choices[3];
+  }
 }
 
 startQuestions();
