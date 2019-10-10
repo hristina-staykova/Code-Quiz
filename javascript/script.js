d5f8588aea0cc7be30ca0ba01b388b498cbe8de1
Hristina Staykova <hristina.staykova@gmail.com>
2019-10-10 11:24:56 +0200

    function to display one question into the index.html, styling the question & links to .js files

--- javascript/script.js
+++ javascript/script.js
@@ -31,3 +31,17 @@ function setTime() {
 }
 
 setTime();
+
+function startQuestions() {
+  var question = document.getElementById("question");
+  var answerEl1 = document.getElementById("answer1");
+  var answerEl2 = document.getElementById("answer2");
+  var answerEl3 = document.getElementById("answer3");
+  var answerEl4 = document.getElementById("answer4");
+  question.textContent = questions[1].title;
+  answerEl1.textContent = "1. " + questions[1].choices[1];
+  answerEl2.textContent = "2. " + questions[1].choices[2];
+  answerEl3.textContent = "3. " + questions[1].choices[3];
+  answerEl4.textContent = "4. " + questions[1].choices[4];
+}
+startQuestions();
