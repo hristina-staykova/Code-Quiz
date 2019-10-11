4726557a702f05cb2ddd6cb243e5a2aef8d0b9ed
Hristina Staykova <hristina.staykova@gmail.com>
2019-10-11 23:14:51 +0200

    updating previous commit

--- javascript/highscores.js
+++ javascript/highscores.js
@@ -3,13 +3,29 @@ var displScores = document.getElementById("newscore");
 
 resetButton.addEventListener("click", resetFn);
 function resetFn() {
-  event.preventDefault();
   localStorage.clear();
 }
 
+// array of objects for the localStorage? every new entry to be pushed in the array and then displayed element by element
 displScores = {
   Name: localStorage.getItem("name"),
   Score: localStorage.getItem("score")
 };
 
 console.log(displScores);
+
+// working, displaying the values from the localStorage
+function dispHighscores() {
+  var name = localStorage.getItem("name");
+  var score = localStorage.getItem("score");
+
+  if (name && score === null) {
+    return;
+  }
+
+  document.getElementById("name").textContent = name;
+  document.getElementById("score").textContent = score;
+}
+// but the reset button doesn't clear the page.... only the localStorage
+
+dispHighscores();
