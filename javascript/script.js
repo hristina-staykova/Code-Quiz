8762672a956109422a3b48a043ef8531dac20fc1
Hristina Staykova <hristina.staykova@gmail.com>
2019-10-11 14:05:03 +0200

    display - none to html blocks that should be visible later in the quiz

--- javascript/script.js
+++ javascript/script.js
@@ -7,9 +7,14 @@ var answerEl2 = document.getElementById("answer2");
 var answerEl3 = document.getElementById("answer3");
 var answerEl4 = document.getElementById("answer4");
 var choices = document.querySelectorAll(".choices");
-console.log(choices[0]);
+
+// to hide these elements, which to be showed later when needed, below in the code
+document.querySelector(".dispq").style.display = "none";
+document.querySelector(".alldone").style.display = "none";
 
 startButton.addEventListener("click", startQuestions);
+
+// document.querySelector(".list").textContent = null;
 
 function countdown() {
   var secondsLeft = 5;
@@ -26,15 +31,18 @@ function countdown() {
 }
 
 function startQuestions() {
+  document.querySelector(".dispq").style.display = "inherit";
   countdown();
   document.querySelector(".start").textContent = null;
   var i = 0;
   var correct = questions[i].answer;
   question.textContent = questions[i].title;
-  answerEl1.textContent = "1. " + questions[i].choices[0];
-  answerEl2.textContent = "2. " + questions[i].choices[1];
-  answerEl3.textContent = "3. " + questions[i].choices[2];
-  answerEl4.textContent = "4. " + questions[i].choices[3];
+  answerEl1.textContent = questions[i].choices[0];
+  answerEl2.textContent = questions[i].choices[1];
+  answerEl3.textContent = questions[i].choices[2];
+  answerEl4.textContent = questions[i].choices[3];
+
+  console.log(answerEl1.textContent, correct);
 
   // event listeners works (listening):
   answerEl1.addEventListener("click", checkFunction);
@@ -44,7 +52,7 @@ function startQuestions() {
 
   // how to check the answer? how to change to the next question?
   function checkFunction() {
-    if (questions[0].choices[i] === correct) {
+    if (answerEl3.textContent === correct) {
       console.log("yes");
       i++;
     } else {
@@ -55,11 +63,11 @@ function startQuestions() {
   }
 
   // displaying the object - question - choices, answer and text content of the html file - works
-  for (i = 0; i < questions[0].choices.length; i++) {
-    console.log(questions[0].choices[i]);
-    console.log(questions[0].answer);
-    console.log(choices[0].textContent);
-  }
+  // for (i = 0; i < questions[0].choices.length; i++) {
+  //   console.log(questions[0].choices[i]);
+  //   console.log(questions[0].answer);
+  //   console.log(choices[0].textContent);
+  // }
 }
 
 // function saveHighscore() {
