0b8a9e9c8a9e4de651a8c7bf84db5bb0597f6d2b
Hristina Staykova <hristina.staykova@gmail.com>
2019-10-13 21:08:43 +0200

    function for displaying questions dispQuestions and end of the quiz endQuiz, adjusting the rest of the code accordingly

--- javascript/script.js
+++ javascript/script.js
@@ -6,7 +6,11 @@ var answerEl1 = document.getElementById("answer1");
 var answerEl2 = document.getElementById("answer2");
 var answerEl3 = document.getElementById("answer3");
 var answerEl4 = document.getElementById("answer4");
-var choices = document.querySelectorAll(".choices");
+var container = document.querySelector(".dispq");
+var message = document.createElement("p");
+var secondsLeft = 75;
+var timer;
+var i = 0;
 
 // to hide these elements, which to be showed later when needed, below in the code
 document.querySelector(".dispq").style.display = "none";
@@ -18,67 +22,83 @@ function startQuestions() {
   document.querySelector(".dispq").style.display = "inherit";
   countdown();
   document.querySelector(".start").textContent = null;
-  var i = 0;
-  var correct = questions[i].answer;
+  answerEl1.addEventListener("click", function() {
+    checkAnswer(answerEl1);
+  });
+  answerEl2.addEventListener("click", function() {
+    checkAnswer(answerEl2);
+  });
+  answerEl3.addEventListener("click", function() {
+    checkAnswer(answerEl3);
+  });
+  answerEl4.addEventListener("click", function() {
+    checkAnswer(answerEl4);
+  });
+  dispQuestions(0);
+}
+
+function dispQuestions(i) {
   question.textContent = questions[i].title;
   answerEl1.textContent = questions[i].choices[0];
   answerEl2.textContent = questions[i].choices[1];
   answerEl3.textContent = questions[i].choices[2];
   answerEl4.textContent = questions[i].choices[3];
-
-  answerEl1.addEventListener("click", function() {
-    checkAnswer(answerEl1, correct);
-  });
-  answerEl2.addEventListener("click", function() {
-    checkAnswer(answerEl2, correct);
-  });
-  answerEl3.addEventListener("click", function() {
-    checkAnswer(answerEl3, correct);
-  });
-  answerEl4.addEventListener("click", function() {
-    checkAnswer(answerEl4, correct);
-  });
-
-  // how to change to the next question?
 }
 
-function checkAnswer(element, correct) {
+function checkAnswer(element) {
+  correct = questions[i].answer;
   if (element.textContent === correct) {
     displayMsg("Right");
   } else {
-    // secondsLeft = secondsLeft - 10;
+    secondsLeft = secondsLeft - 10;
     displayMsg("Wrong");
+  }
+  i++;
+  if (i === questions.length) {
+    endQuiz();
+  } else {
+    dispQuestions(i);
   }
 }
 
 function displayMsg(messageContent) {
-  var container = document.querySelector(".dispq");
-  var message = document.createElement("p");
   message.style.fontStyle = "italic";
   message.textContent = messageContent;
   container.appendChild(message);
 }
 
 function countdown() {
-  var secondsLeft = 5;
-  var timer = setInterval(myTimer, 1000);
+  secondsEl.textContent = secondsLeft;
+  timer = setInterval(myTimer, 1000);
   function myTimer() {
     secondsLeft--;
     secondsEl.textContent = secondsLeft;
-    if (secondsLeft === 0) {
-      document.querySelector(".dispq").style.display = "none";
-      document.querySelector(".alldone").style.display = "inherit";
-      console.log("Game over");
-      clearInterval(timer);
+    if (secondsLeft <= 0) {
+      secondsEl.textContent = 0;
+      endQuiz();
     }
   }
+}
+
+function endQuiz() {
+  clearInterval(timer);
+  document.querySelector(".dispq").style.display = "none";
+  document.querySelector(".alldone").style.display = "inherit";
+  console.log(secondsLeft);
 }
 
 // submit new highscore and save it to localStorage
 nhsButton.addEventListener("click", function(event) {
   event.preventDefault();
   var score = document.getElementById("score").textContent;
-  var initials = document.getElementById("initials").value;
-  localStorage.setItem("name", initials);
-  localStorage.setItem("score", score);
+  var initials = document.getElementById("initials").value.trim();
+  var newscore = {
+    name: initials,
+    score: score
+  };
+  console.log(newscore);
+  localStorage.setItem("newscore", JSON.stringify(newscore));
+
+  // localStorage.setItem("name", initials);
+  // localStorage.setItem("score", score);
 });
